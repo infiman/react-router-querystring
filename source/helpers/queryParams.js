a1e89ccfeb8961b5452963e9c357578fb138c86c
Igor Shabat <shabat.igor@gmail.com>
2019-05-11 19:15:30 +0300

    Used deepMerge in queryParams

--- source/helpers/queryParams.js
+++ source/helpers/queryParams.js
@@ -1,61 +1,23 @@
 import memoize from 'fast-memoize'
 
+import { mergeDeep } from './'
+
 export const addQueryParams = memoize((queryParams, params) =>
-  Object.keys(params || {}).reduce(
-    (destination, key) => {
-      const target = destination[key]
-      const patch = params[key]
-      const payload = {}
+  mergeDeep(queryParams, params, (oldValue, newValue) => {
+    if (Array.isArray(oldValue) && Array.isArray(newValue)) {
+      return [...oldValue, ...newValue]
+    }
 
-      if (Array.isArray(patch) && Array.isArray(target)) {
-        payload[key] = [...target, ...patch]
-      } else if (
-        patch &&
-        typeof patch === 'object' &&
-        target &&
-        typeof target === 'object'
-      ) {
-        payload[key] = addQueryParams(target, patch)
-      } else if (typeof patch !== 'undefined') {
-        payload[key] = patch
-      } else {
-        payload[key] = target
-      }
-
-      return {
-        ...destination,
-        ...payload
-      }
-    },
-    { ...(queryParams || {}) }
-  )
+    return newValue
+  })
 )
 
 export const removeQueryParams = memoize((queryParams, params) =>
-  Object.keys(params || {}).reduce(
-    (destination, key) => {
-      const target = queryParams[key]
-      const patch = params[key]
-      const payload = {}
+  mergeDeep(queryParams, params, (oldValue, newValue) => {
+    if (Array.isArray(oldValue) && Array.isArray(newValue)) {
+      return oldValue.filter((item, i) => !newValue.includes(item))
+    }
 
-      if (Array.isArray(patch) && Array.isArray(target)) {
-        payload[key] = target.filter((item, i) => !patch.includes(item))
-      } else if (
-        patch &&
-        typeof patch === 'object' &&
-        target &&
-        typeof target === 'object'
-      ) {
-        payload[key] = removeQueryParams(target, patch)
-      } else if (typeof patch === 'undefined') {
-        delete destination[key]
-      }
-
-      return {
-        ...destination,
-        ...payload
-      }
-    },
-    { ...(queryParams || {}) }
-  )
+    return newValue
+  })
 )
